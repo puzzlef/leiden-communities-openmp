232e6da857cf35e1a26c86dd8357759fa9fb898e
Subhajit Sahu <wolfram77@gmail.com>
2023-12-17 16:14:45 +0530

    :bug: process output logs

--- process.js
+++ process.js
@@ -5,7 +5,7 @@ const path = require('path');
 const ROMPTH = /^OMP_NUM_THREADS=(\d+)/;
 const RGRAPH = /^Loading graph .*\/(.*?)\.mtx \.\.\./m;
 const RORDER = /^order: (\d+) size: (\d+) (?:\[\w+\] )?\{\}/m;
-const RRESLT = /^\{(.+?)ms, (.+?)ms preproc, (.+?)ms firstpass, (.+?)ms locmove, (.+?)ms aggr, (.+?) iters, (.+?) passes, (.+?) modularity, (.+?)\/(.+?) disconnected\} (.+)/m;
+const RRESLT = /^\{(.+?)ms, (.+?)ms mark, (.+?)ms init, (.+?)ms firstpass, (.+?)ms locmove, (.+?)ms refine, (.+?)ms aggr, (.+?) aff, (.+?) iters, (.+?) passes, (.+?) modularity, (.+?)\/(.+?) disconnected\} (.+)/m;
 
 
 
@@ -60,13 +60,16 @@ function readLogLine(ln, data, state) {
     state.size  = parseFloat(size);
   }
   else if (RRESLT.test(ln)) {
-    var [, time, preprocessing_time, first_pass_time, local_moving_phase_time, aggregation_phase_time, iterations, passes, modularity, disconnected_communities, total_communities, technique] = RRESLT.exec(ln);
+    var [, time, marking_time, initialization_time, first_pass_time, local_moving_phase_time, refinement_phase_time, aggregation_phase_time, affected_vertices, iterations, passes, modularity, disconnected_communities, total_communities, technique] = RRESLT.exec(ln);
     data.get(state.graph).push(Object.assign({}, state, {
       time:        parseFloat(time),
-      preprocessing_time:      parseFloat(preprocessing_time),
+      marking_time:            parseFloat(marking_time),
+      initialization_time:     parseFloat(initialization_time),
       first_pass_time:         parseFloat(first_pass_time),
       local_moving_phase_time: parseFloat(local_moving_phase_time),
+      refinement_phase_time:   parseFloat(refinement_phase_time),
       aggregation_phase_time:  parseFloat(aggregation_phase_time),
+      affected_vertices:       parseFloat(affected_vertices),
       iterations:  parseFloat(iterations),
       passes:      parseFloat(passes),
       modularity:  parseFloat(modularity),
